596d0a30f4dce22268b7f59de0b4f4afb0c90884
Nico Bykhovsky <nico@bykhovsky.com>
2024-08-19 16:47:24 -0400

    pushing mixpanel

--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -10,7 +10,7 @@ import datetime
 from flask_cors import cross_origin
 import logging
 from flask_mail import Message
-from mixpanel_utils import *
+from src.routes.mixpanel_utils import *
 import datetime
 import random
 import string
@@ -221,8 +221,8 @@ def login():
     user_details = {key: str(value) if isinstance(value, ObjectId) else value for key, value in user_details.items()}
     access_token = create_access_token(identity=user['username'], additional_claims=user_details)
 
-    track_event(user['_id'], 'login', {'email': email, 'time': datetime.datetime.utcnow()})
-    set_user_profile(user['_id'], {'name': user['username'], 'email': user['email'], 'last_login': datetime.datetime.utcnow()})
+    track_event(str(user['_id']), 'login', {'email': email, 'time': datetime.datetime.utcnow()})
+    set_user_profile(str(user['_id']), {'name': user['username'], 'email': user['email'], 'last_login': datetime.datetime.utcnow()})
     return jsonify(access_token=access_token), 200
 
 @user_bp.route('/studentProfileEditor', methods=['GET', 'POST'])
